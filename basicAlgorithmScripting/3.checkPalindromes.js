fad1b6e4856058297b14246098a03e2f3a7c174f
grzegorzzajac1989 <zajacgrzegorz1989@gmail.com>
2016-08-10 13:05:29 +0200

    Rename scripts in basicAlgorithmScripting

---
+++ basicAlgorithmScripting/3.checkPalindromes.js
@@ -0,0 +1,11 @@
+function palindrome(str) {
+
+  var strippedStr = str.toLowerCase().replace(/\W|_/g, '');
+  var reversedStr = strippedStr.split('').reverse().join('');
+
+  if(reversedStr === strippedStr) {
+    return true;
+  } else {
+    return false;
+  }
+}
