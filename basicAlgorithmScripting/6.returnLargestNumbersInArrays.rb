943a5520acef074ac3358cb19ea94b6a698dc5f0
grzegorzzajac1989 <zajacgrzegorz1989@gmail.com>
2016-08-27 16:27:05 +0200

    improvement ruby convention code

--- basicAlgorithmScripting/6.returnLargestNumbersInArrays.rb
+++ basicAlgorithmScripting/6.returnLargestNumbersInArrays.rb
@@ -1,14 +1,14 @@
 
 def largestOfFour(arr)
 
-final = Array.new
+  final = Array.new
+  n = 0
 
-n = 0
-while n < 4 do
-final.push(arr[n].sort.last)
-n += 1
-end
-puts final
+  while n < 4 do
+    final.push(arr[n].sort.last)
+    n += 1
+  end
+  puts final
 end
 
 largestOfFour( [[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]] )
